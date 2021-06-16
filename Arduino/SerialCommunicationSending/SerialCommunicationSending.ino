3f2b7152866093bb71d8c1d797da9a6cf1969fa2
Bas <b.g.vandersteenhoven@student.utwente.nl>
2021-06-16 13:54:16 +0200

    Update SerialCommunicationSending.ino
    Added a method for the water valves. Also added distance sensor functionality.

--- Arduino/SerialCommunicationSending/SerialCommunicationSending.ino
+++ Arduino/SerialCommunicationSending/SerialCommunicationSending.ino
@@ -8,18 +8,42 @@
 // Communcation headers
 char header[] = {'A', 'B', 'C', 'D'};
 
-//TEST
-#define yellow_LED 12
-#define red_LED 13
-#define green_LED 14
+
+//--------------------------------Water Valves---------------------------------//
+// Water valve Pins
+#define valvePin_1_Out 22
+#define valvePin_1_In 23
+#define valvePin_2_Out 24
+#define valvePin_2_In 25
+#define valvePin_3_Out 26
+#define valvePin_3_In 27
+#define valvePin_4_Out 28
+#define valvePin_4_In 29
+#define valvePin_5_Out 30
+#define valvePin_5_In 31
+#define valvePin_6_Out 32
+#define valvePin_6_In 33
 
 
-//--------------------------------Distance Sensor---------------------------------------------//
+
+//--------------------------------Distance Sensor------------------------------//
 // Distance Sensor Pins
-#define trigPinA 5
-#define echoPinA 6
-#define trigPinB 7
-#define echoPinB 8
+#define trigPinA 44
+#define echoPinA 45
+#define trigPinB 46
+#define echoPinB 47
+#define trigPinC 48
+#define echoPinC 49
+#define trigPinD 50
+#define echoPinD 51
+#define trigPinE 52
+#define echoPinE 53
+
+int oldDistanceA;
+int oldDistanceB;
+
+unsigned long distanceTimer;
+
 
 //--------------------------------Rotary_Encoder-------------------------------//
 // Rotary Encoder Inputs
@@ -59,8 +83,13 @@ void setup() {
 void loop() {
   encoderReader();
   buttonDetection();
-  distanceRead();
-  //Serial.println("Sukkel");
+
+  if (millis() > distanceTimer + 333) {
+    distanceRead();
+    distanceTimer = millis();
+    //Serial.print('Z');
+    //Serial.println("ukkel");
+  }
 
   if (Serial.available() > 0) {
     ledReadTest();
@@ -68,7 +97,7 @@ void loop() {
   delay(1);
 }
 
-
+// Communication Test
 void ledReadTest() {                                              // Read the data
   String recievedData = Serial.readStringUntil('\n');
   Serial.print("E");
@@ -80,28 +109,86 @@ void ledReadTest() {                                              // Read the da
   switch (recieveHeader) {
     case 'A':
       if (data == 1) {
-        digitalWrite(yellow_LED, HIGH);
+        digitalWrite(valvePin_1_Out, HIGH);
       } else {
-        digitalWrite(yellow_LED, LOW);
+        digitalWrite(valvePin_1_Out, LOW);
       }
       break;
     case 'B':
       if (data == 1) {
-        digitalWrite(red_LED, HIGH);
+        digitalWrite(valvePin_1_In, HIGH);
       } else {
-        digitalWrite(red_LED, LOW);
+        digitalWrite(valvePin_1_In, LOW);
       }      break;
     case 'C':
       if (data == 1) {
-        digitalWrite(green_LED, HIGH);
+        digitalWrite(valvePin_2_Out, HIGH);
       } else {
-        digitalWrite(green_LED, LOW);
+        digitalWrite(valvePin_2_Out, LOW);
+      }      break;
+    case 'D':
+      if (data == 1) {
+        digitalWrite(valvePin_2_In, HIGH);
+      } else {
+        digitalWrite(valvePin_2_In, LOW);
+      }
+      break;
+    case 'E':
+      if (data == 1) {
+        digitalWrite(valvePin_3_Out, HIGH);
+      } else {
+        digitalWrite(valvePin_3_Out, LOW);
+      }      break;
+    case 'F':
+      if (data == 1) {
+        digitalWrite(valvePin_3_In, HIGH);
+      } else {
+        digitalWrite(valvePin_3_In, LOW);
+      }      break;
+    case 'G':
+      if (data == 1) {
+        digitalWrite(valvePin_4_Out, HIGH);
+      } else {
+        digitalWrite(valvePin_4_Out, LOW);
+      }
+      break;
+    case 'H':
+      if (data == 1) {
+        digitalWrite(valvePin_4_In, HIGH);
+      } else {
+        digitalWrite(valvePin_4_In, LOW);
+      }      break;
+    case 'I':
+      if (data == 1) {
+        digitalWrite(valvePin_5_Out, HIGH);
+      } else {
+        digitalWrite(valvePin_5_Out, LOW);
+      }      break;
+    case 'J':
+      if (data == 1) {
+        digitalWrite(valvePin_5_In, HIGH);
+      } else {
+        digitalWrite(valvePin_5_In, LOW);
+      }
+      break;
+    case 'K':
+      if (data == 1) {
+        digitalWrite(valvePin_6_Out, HIGH);
+      } else {
+        digitalWrite(valvePin_6_Out, LOW);
+      }      break;
+    case 'L':
+      if (data == 1) {
+        digitalWrite(valvePin_6_In, HIGH);
+      } else {
+        digitalWrite(valvePin_6_In, LOW);
       }      break;
   }
 
 }
 
 
+// Detects the button of the rotary encoder
 void buttonDetection() {
   // Read the button state
   int btnState = digitalRead(ROT_ENC_SW);
@@ -124,6 +211,7 @@ void buttonDetection() {
 }
 
 
+// Reads the encoder and outputs that value to the Pi
 void encoderReader() {
   // Read the current state of CLK
   currentStateCLK = digitalRead(ROT_ENC_CLK);
@@ -146,14 +234,25 @@ void encoderReader() {
 }
 
 
+// Reads the distance sensors and outputs their data to the Pi
 void distanceRead() {
-  Serial.print("E");
-  Serial.println(distance(echoPinA, trigPinA));
 
-  Serial.print("F");
-  Serial.println(distance(echoPinB, trigPinB));
+  int tempDistance = distance(echoPinA, trigPinA);
+  if (tempDistance != oldDistanceA) {
+    Serial.print("E");
+    Serial.println(tempDistance);
+  }
+  oldDistanceA = tempDistance;
+
+  tempDistance = distance(echoPinB, trigPinB);
+  if (tempDistance != oldDistanceB) {
+    Serial.print("F");
+    Serial.println(tempDistance);
+  }
+  oldDistanceB = tempDistance;
 }
 
+// Calculates the distance for the distance sensors
 int distance(int echoPin, int trigPin) {
   int duration, distance;
   digitalWrite (trigPin, HIGH);
